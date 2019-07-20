1c640c1208040fe03e090bf83802d295ad02279c
Ckiddo <815642475@qq.com>
2019-07-20 12:45:19 +0800

    add DbFactory

---
+++ src/com/icss/wind/util/DbFactory.java
@@ -0,0 +1,19 @@
+package com.icss.wind.util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class DbFactory {
+    private static final ThreadLocal<Connection> tl = new ThreadLocal<Connection>();
+    public static Connection openConnection() throws Exception{
+        Connection connection = tl.get();
+        if(connection == null || connection.isClosed()){
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/","root","heimeiGUI123");
+            tl.set(connection);
+        }
+        return connection;
+    }
+}
